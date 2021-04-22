65109faf91abff6b218349d4cde299136a2db62a
Liaisan Akhmetova <akhmetova1423@gmail.com>
2021-04-22 13:19:12 +0300

    more methods

--- src/data_structure.cpp
+++ src/data_structure.cpp
@@ -18,7 +18,30 @@ namespace itis {
       FenwickTree::updateFW(fw, size_, i, data_[i]);
 
     return fw;
-  }
+
+    //сумма элементов от l до r
+    int FenwickTree::getSum(int *fw, int l, int r) {
+      if (l) {
+        return getSumFromZero(fw, r) - getSumFromZero(fw, l - 1);
+      } else {
+        return getSumFromZero(fw, r);
+      }
+    }
+//сумма элементов от 0 до x
+    int FenwickTree::getSumFromZero(int *fw, int i) {
+      int sum = 0;
+// Fenwick's index start from 1
+      i++;
+
+      while (i > 0) {
+        sum += fw[i];
+// i & (-i) returns the decimal value of last set digit
+// eg: if i = 12 (1100) then i & (-i) will 4 (100)
+        i -= i & (-i);
+      }
+      return sum;
+    }
+
   //увеличение fx[i] на newVal
   void FenwickTree::updateFW(int *fw, int n, int i, int newVal) {
     // Fenwick's index start from 1
